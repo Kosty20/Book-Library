86117090493063db6ca4c7540f8d02ff2cc16148
Kosty20 <104622649+Kosty20@users.noreply.github.com>
2023-01-29 14:28:02 +0200

    Minor update

---
+++ code.js
@@ -0,0 +1,9 @@
+function Book (title, author, pageNum, finished) {
+    this.title = title;
+    this.author = author;
+    this.pageNum = pageNum;
+    this.finished = finished;
+}
+Book.prototype.info = function () {
+    return `${this.title} by ${this.author}, ${this.pageNum} pages, ${this.finished ? 'finished reading' : 'not read yet'}`;
+}
