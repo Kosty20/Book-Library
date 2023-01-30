b205cd108ee5e9e2c40b246ec1a0d80fc04cca01
Kosty20 <104622649+Kosty20@users.noreply.github.com>
2023-01-30 17:28:03 +0200

    Finished styling the page

--- code.js
+++ code.js
@@ -1,3 +1,6 @@
+
+let library = [];
+
 function Book (title, author, pageNum, finished) {
     this.title = title;
     this.author = author;
@@ -7,3 +10,8 @@ function Book (title, author, pageNum, finished) {
 Book.prototype.info = function () {
     return `${this.title} by ${this.author}, ${this.pageNum} pages, ${this.finished ? 'finished reading' : 'not read yet'}`;
 }
+
+function addBooks() {
+    library.push(Book());
+}
+
